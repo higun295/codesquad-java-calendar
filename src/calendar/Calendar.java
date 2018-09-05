4e45ba940c2c823dac6028b0436e8bdbd74f933f
wato <wato@192.168.1.24>
2018-09-05 15:37:10 +0700

    Calendar Project

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,8 +3,7 @@ package calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello, Calendar");
+		System.out.println("Hello, Calendar");
 	}
 
 }
