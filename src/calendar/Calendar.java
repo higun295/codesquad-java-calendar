236a373bba07441ace6ef8b9ad6133abf6d8255c
wato <wato@192.168.1.24>
2018-09-05 16:34:41 +0700

    Update Using Class Method

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,21 +4,30 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	public static void main(String[] args) {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int maxDaysOfMonth(int monthNum) {
+		return MAX_DAYS[monthNum - 1];
+	}
+
+	public void printSampleCalendar() {
 		System.out.println("일    월    화   수    목    금    토");
 		System.out.println("-------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+	}
+
+	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		System.out.println("달을 입력하세요.");
+		Calendar cal = new Calendar();
 		Scanner sc = new Scanner(System.in);
 		int monthNum = sc.nextInt();
 
-		int[] month = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		System.out.println(monthNum + "월은 " + month[monthNum - 1] + "일까지 있습니다.");
+		System.out.println(monthNum + "월은 " + cal.maxDaysOfMonth(monthNum) + "일까지 있습니다.");
 
 		sc.close();
 	}
